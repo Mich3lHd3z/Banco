1629630f01b73090f4a61c33e40e1ca9518bd852
Mich3lHd3z <2030109@upt.edu.mx>
2023-07-19 13:31:14 -0600

    prueba

--- api/APICASA/conn.php
+++ api/APICASA/conn.php
@@ -1,5 +1,21 @@
 <?php
-
 //!Apartado para la conexión con la base de datos
- 
+class conexion extends PDO
+{
+	private $hostBd='localhost';
+	private $NombreBd='datos';
+	private $usuarioBd='root';
+	private $passwordBd='';
+	
+	
+public function __construct(){
+	try{
+		parent::__construct('mysql:host=' . $this->hostBd . ';dbname=' . $this->NombreBd . ';charset=utf8', $this->usuarioBd, $this->passwordBd, array (PDO:: ATTR_ERRMODE =>PDO:: ERRMODE_EXCEPTION));
+		
+	}catch(PDOException $e){
+		echo'Error: '.$e->getMessage();
+		exit;
+    }
+    }	
+}	
 ?>
