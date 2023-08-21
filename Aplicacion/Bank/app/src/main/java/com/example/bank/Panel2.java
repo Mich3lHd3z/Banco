ff7dfb7a35668e7ca08390e503c877ffeab66d9e
IvanIslasAmador <137328437+IvanIslasAmador@users.noreply.github.com>
2023-08-21 00:15:54 -0600

    Actualizacion

--- Aplicacion/Bank/app/src/main/java/com/example/bank/Panel2.java
+++ Aplicacion/Bank/app/src/main/java/com/example/bank/Panel2.java
@@ -12,6 +12,7 @@ import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -79,7 +80,7 @@ public class Panel2 extends AppCompatActivity {
                     try {
                         JSONObject jsonObject = new JSONObject(response);
 
-                        String rol = jsonObject.getString("Rol");
+                        String rol = jsonObject.getString("Clave");
 
                     } catch (JSONException e) {
                         e.printStackTrace();
