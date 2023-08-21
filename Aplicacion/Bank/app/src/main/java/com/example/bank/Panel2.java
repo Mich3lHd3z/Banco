2a04e305fa896e507aac55d8b9104de778de507f
EstebanVz <1930445@upt.edu.mx>
2023-08-21 00:05:56 -0600

    Panel2
    Arsegla errores

--- Aplicacion/Bank/app/src/main/java/com/example/bank/Panel2.java
+++ Aplicacion/Bank/app/src/main/java/com/example/bank/Panel2.java
@@ -1,11 +1,19 @@
 package com.example.bank;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Panel2 extends AppCompatActivity {
 
@@ -37,4 +45,57 @@ public class Panel2 extends AppCompatActivity {
     public void onRegresarClick(View view) {
         finish(); // Finaliza la actividad actual y regresa a la actividad anterior en la pila
     }
+
+
+    public class P_LOGIN extends AppCompatActivity {
+
+        TextView textView;
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_panel2);
+
+            Button sendButton = findViewById(R.id.sendButton);
+            textView= findViewById(R.id.textView);
+
+
+            sendButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    _Consulta(textView.getText().toString());
+                }
+            });
+
+        }
+
+
+        private void _Consulta(final String _Clave) {
+            String url = "https://proyectos123tra.000webhostapp.com/Banco/boveda.php?Valor=" + _Clave;
+
+            StringRequest postRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    try {
+                        JSONObject jsonObject = new JSONObject(response);
+
+                        String rol = jsonObject.getString("Rol");
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e("Error", error.getMessage());
+                }
+            });
+
+            Volley.newRequestQueue(this).add(postRequest);
+        }
+
+
+    }
+
 }
